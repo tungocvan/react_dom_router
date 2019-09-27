4cb83e7874ba4c0807dbefba4c0f3968a0a89c8f
tungocvan <tungocvan@gmail.com>
2019-09-27 21:01:37 +0700

    demo-react-router

---
+++ precache-manifest.e386db6958794980bd4a57932fea995e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec6219521115add2253ea3e8e0a5155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcfba4c649c3d3244c67",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ef171ae85fc1d58f74ed",
+    "url": "/static/js/2.10cf3887.chunk.js"
+  },
+  {
+    "revision": "dcfba4c649c3d3244c67",
+    "url": "/static/js/main.8dc943b3.chunk.js"
+  },
+  {
+    "revision": "835cae36aaa8e6a9c8db",
+    "url": "/static/js/runtime~main.5b93e505.js"
+  }
+]);
